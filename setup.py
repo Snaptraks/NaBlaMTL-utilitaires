9729eb3fdd8047ff1433785cb9f644238b87d0da
François Hardy <snaptraks@gmail.com>
2019-11-14 11:19:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='NaBlaUtils',
     version='0.2',
-    description="Utilitaires Python pour l'analyse et la visualisation de données relatives aux étoiles naines blanches",
-    url='https://github.com/simonblouin/NaBlaMTL-utilitaires',
+    description="Python utility for data analysis and visualization of related to white dwarfs.",
+    url='https://github.com/Snaptraks/NaBlaMTL-utilitaires',
     package_data={'': ['lines.csv']},
     packages=find_packages()
     )
