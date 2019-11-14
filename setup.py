6831f32edd142d141926b910aa7072753eaec50e
François Hardy <snaptraks@gmail.com>
2019-11-14 11:00:56 -0500

    remove MWDD table requirement

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1',
     description="Utilitaires Python pour l'analyse et la visualisation de données relatives aux étoiles naines blanches",
     url='https://github.com/simonblouin/NaBlaMTL-utilitaires',
-    package_data={'': ['lines.csv', 'MWDD_table.json']},
+    package_data={'': ['lines.csv']},
     packages=find_packages()
     )
