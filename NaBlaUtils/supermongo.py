5a3160a6840dde15ebae48dd68e8de7ddb32923c
François Hardy <snaptraks@gmail.com>
2020-01-15 16:13:39 -0500

    Update rc_context and sort alphabetically

--- NaBlaUtils/supermongo.py
+++ NaBlaUtils/supermongo.py
@@ -7,18 +7,8 @@ def supermongo():
     library. Font is still not quite there yet.
     """
     return rc_context({
-        'text.usetex' : True,
-
-        'figure.figsize': (6, 6),
-        'figure.facecolor': 'none',
-        'figure.edgecolor': 'none',
-        'figure.autolayout': True,
-
-        'font.size' : 11,
-        'font.family': 'serif',
-
-        'axes.labelsize': 'xx-large',
         'axes.facecolor': 'none',
+        'axes.labelsize': 'large',
         'axes.linewidth': 0.7,
         'axes.prop_cycle': cycler(color=[
             '#FF0000',
@@ -30,34 +20,59 @@ def supermongo():
             '#000000',
             ]),
 
-        'xtick.labelsize': 'medium',
-        'xtick.top': True,
-        'xtick.bottom': True,
-        'xtick.direction': 'in',
-        'xtick.major.size': 8,
-        'xtick.minor.visible': True,
-        'xtick.minor.size': 3,
+        'backend': 'GTK3AGG',
 
-        'ytick.labelsize': 'medium',
+        'figure.autolayout': True,
+        'figure.edgecolor': 'none',
+        'figure.facecolor': 'none',
+        'figure.figsize': (6, 6),
+
+        'font.family': 'serif',
+        'font.size' : 11,
+        'font.stretch': 'extra-expanded',
+        'font.weight': 'bold',
+
+        'legend.frameon': False,
+        'legend.handleheight': 0.1,
+        'legend.handlelength': 2,
+        'legend.handletextpad': 0.3,
+        'legend.numpoints': 1,
+        'legend.scatterpoints': 1,
+
+        'lines.linewidth': 0.8,
+        'lines.markeredgewidth': 0.2,
+        'lines.markersize': 4,
+
+        'mathtext.cal': 'cursive',
+        'mathtext.fontset': 'cm',
+
+        'savefig.dpi': 300,
+        'savefig.edgecolor': 'none',
+        'savefig.facecolor': 'none',
+
+        'text.usetex' : True,
+
+        'xtick.bottom': True,
+        'xtick.top': True,
+        'xtick.direction': 'in',
+        'xtick.labelsize': 'medium',
+        'xtick.major.pad': 6,
+        'xtick.major.size': 8,
+        'xtick.major.width': 1,
+        'xtick.minor.pad': 6,
+        'xtick.minor.size': 3,
+        'xtick.minor.visible': True,
+        'xtick.minor.width': 1,
+
         'ytick.left': True,
         'ytick.right': True,
         'ytick.direction': 'in',
+        'ytick.labelsize': 'medium',
+        'ytick.major.pad': 6,
         'ytick.major.size': 8,
-        'ytick.minor.visible': True,
+        'ytick.major.width': 1,
+        'ytick.minor.pad': 6,
         'ytick.minor.size': 3,
-
-        'lines.markersize': 4,
-        'lines.linewidth': 0.8,
-        'lines.markeredgewidth': 0.2,
-
-        'legend.numpoints': 1,
-        'legend.frameon': False,
-        'legend.handletextpad': 0.3,
-        'legend.scatterpoints': 1,
-        'legend.handlelength': 2,
-        'legend.handleheight': 0.1,
-
-        'savefig.dpi': 300,
-        'savefig.facecolor': 'none',
-        'savefig.edgecolor': 'none',
+        'ytick.minor.visible': True,
+        'ytick.minor.width': 1,
         })
